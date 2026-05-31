1091afa415d6c1dcd3dbdac898555b0ccb115cad
Owen Montiel Cardeña <139807171+Owen117@users.noreply.github.com>
2026-05-31 15:08:44 -0500

    Delete firmware directory

--- firmware/src/drivers/servo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include "../include/error_codes.h"
-
-void servo_init(void);
-asrs_err_t servo_set_angle(uint8_t degrees);
-asrs_err_t servo_extend(void);
-asrs_err_t servo_retract(void);
